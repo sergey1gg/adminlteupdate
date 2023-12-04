72e67defd1693d02f0ca743d39778f50051476e5
WendemBox <stepanfrolov1602@gmail.com>
2023-12-04 23:12:14 +0300

    timeFix

--- src/actions/stats-api.js
+++ src/actions/stats-api.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const server=process.env.REACT_APP_SERVER_URL;
 export const kiosks_orders_report = async(from, to) => {
       try {
-        const response = await axios.get(`${server}/kiosk/orders_report?date_from=${from}&date_to=${to}`,
+        const response = await axios.get(`${server}/kiosk/orders_report?date_from=${from}T00:00:00&date_to=${to}T23:59:59`,
           {
             headers: { Authorization: `Bearer ${localStorage.getItem('access_token')}` }
           })
@@ -15,7 +15,7 @@ export const kiosks_orders_report = async(from, to) => {
 
 export const kiosks_items_report = async(from, to) => {
   try {
-    const response = await axios.get(`${server}/kiosk/items_report?date_from=${from}&date_to=${to}`,
+    const response = await axios.get(`${server}/kiosk/items_report?date_from=${from}T00:00:00&date_to=${to}T23:59:59`,
       {
         headers: { Authorization: `Bearer ${localStorage.getItem('access_token')}` }
       })
@@ -27,7 +27,7 @@ export const kiosks_items_report = async(from, to) => {
 
 export const kiosks_total_report = async(from, to) => {
   try {
-    const response = await axios.get(`${server}/kiosk/kiosk_total_report?date_from=${from}&date_to=${to}`,
+    const response = await axios.get(`${server}/kiosk/kiosk_total_report?date_from=${from}T00:00:00&date_to=${to}T23:59:59`,
       {
         headers: { Authorization: `Bearer ${localStorage.getItem('access_token')}` }
       })
